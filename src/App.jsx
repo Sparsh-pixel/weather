009c3eb8617f12c85e27c72134ae6e331a905494
Sparsh-pixel <sparshprasad7@gmail.com>
2024-07-25 11:31:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Weather from "./components/Weather";
+import "./index.css"
+function App() {
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
